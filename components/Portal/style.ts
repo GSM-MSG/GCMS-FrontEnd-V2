f8a490ef24f3c25974b5b66e0ab4dd43e8f1cf9c
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-02-01 23:01:48 +0900

    Add Portal component

---
+++ components/Portal/style.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled'
+
+export const Wrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background: #000;
+  opacity: 0.64;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`

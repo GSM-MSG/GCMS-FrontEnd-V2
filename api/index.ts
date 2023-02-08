5d5feeb93e20380880dda68bb322fb1e1e6ba945
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-02-08 22:30:26 +0900

    Remove cors config

--- api/index.ts
+++ api/index.ts
@@ -2,13 +2,8 @@ import axios from 'axios'
 import TokenManager from './TokenManager'
 
 const API = axios.create({
-  baseURL: process.env.SERVER_URL,
+  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
   withCredentials: true,
-  headers: {
-    'Access-Control-Request-Method':
-      'GET, PUT, POST, PATCH, DELETE, HEAD, OPTIONS',
-    'Access-Control-Request-Headers': 'Content-Type, Authorization',
-  },
 })
 
 API.interceptors.request.use(async (config) => {

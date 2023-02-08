2ea010b347c249d3bd82373e74da76258a7edec6
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-02-08 22:16:24 +0900

    Add TokenManager to axios interceptor

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import TokenManager from './TokenManager'
 
 const API = axios.create({
   baseURL: process.env.SERVER_URL,
@@ -8,6 +9,22 @@ const API = axios.create({
       'GET, PUT, POST, PATCH, DELETE, HEAD, OPTIONS',
     'Access-Control-Request-Headers': 'Content-Type, Authorization',
   },
+})
+
+API.interceptors.request.use(async (config) => {
+  const tokenManager = new TokenManager()
+
+  if (
+    !tokenManager.validateToken(
+      tokenManager.accessExp,
+      tokenManager.accessToken
+    )
+  )
+    await tokenManager.tokenReissue()
+
+  config.headers['Authorization'] = tokenManager.accessToken
+
+  return config
 })
 
 export default API
---
+++ lib/token.ts
@@ -0,0 +1,4 @@
+export const accessToken = 'GCMS-accessToken'
+export const refreshToken = 'GCMS-refreshToken'
+export const accessExp = 'GCMS-accessExp'
+export const refreshExp = 'GCMS-refreshExp'

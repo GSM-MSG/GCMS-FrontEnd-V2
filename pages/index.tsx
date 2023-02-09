e15f7f07ddfbed652d2922507a66f8002aebf7b0
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-02-10 00:05:07 +0900

    Reset page/index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,3 @@
-import ClubCreationModal from '@/components/ClubCreationModal'
-
 export default function Home() {
-  // return <>hello world</>
-  return (
-    <ClubCreationModal
-      onClose={() => {
-        return
-      }}
-    />
-  )
+  return <>hello world</>
 }

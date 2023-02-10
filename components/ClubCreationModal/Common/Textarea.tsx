b2ee7aa7b064eec3d69860c9960efe392ee804f3
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-02-10 13:59:57 +0900

    Use react-hook-form

--- components/ClubCreationModal/Common/Textarea.tsx
+++ components/ClubCreationModal/Common/Textarea.tsx
@@ -1,12 +1,23 @@
+import { ContentFormType } from '@/type/components/ClubCreationModal'
+import { UseFormRegisterReturn, UseFormWatch } from 'react-hook-form'
 import * as S from './Textarea.style'
 
-const Textarea = () => {
+interface Props {
+  register: UseFormRegisterReturn
+  watch: UseFormWatch<ContentFormType>
+  error: boolean
+}
+
+const Textarea = ({ register, watch }: Props) => {
   return (
     <S.Wrapper>
       <S.Label>동아리 설명</S.Label>
       <div>
-        <S.TextareaInput placeholder='동아리 설명을 입력해주세요.' />
-        <S.MaxLength>10/200</S.MaxLength>
+        <S.TextareaInput
+          placeholder='동아리 설명을 입력해주세요.'
+          {...register}
+        />
+        <S.MaxLength>{watch('content').length}/200</S.MaxLength>
       </div>
     </S.Wrapper>
   )
--- components/ClubCreationModal/Page/ClubContent.tsx
+++ components/ClubCreationModal/Page/ClubContent.tsx
@@ -1,16 +1,27 @@
+import { ContentFormType } from '@/type/components/ClubCreationModal'
 import { useForm } from 'react-hook-form'
 import Layout from '../Common/Layout'
 import TextareaInput from '../Common/Textarea'
 
 const ClubContent = () => {
-  const {} = useForm()
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<ContentFormType>()
+
   const onSubmit = async () => {
     return
   }
 
   return (
-    <Layout onSubmit={onSubmit} back>
-      <TextareaInput />
+    <Layout onSubmit={handleSubmit(onSubmit)} back>
+      <TextareaInput
+        register={register('content', { required: true, maxLength: 200 })}
+        watch={watch}
+        error={!!errors.content}
+      />
     </Layout>
   )
 }

e644e06e151d38a0155ffbbf5ac0be8ca930b5f3
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-02-11 13:38:54 +0900

    Fix msw api /user/search

--- mocks/handlers/api/user/search/get.ts
+++ mocks/handlers/api/user/search/get.ts
@@ -1,29 +1,32 @@
 import serverApi from '@/mocks/handlers/serverApi'
 import { rest } from 'msw'
 
-const getMain = rest.get(serverApi('/user/search'), (_req, res, ctx) => {
-  return res(
-    ctx.json([
-      {
-        uuid: 'sdjfksdlfjskf',
-        email: 's21025',
-        name: '김준',
-        grade: 2,
-        classNum: 1,
-        number: 5,
-        profileImg: '',
-      },
-      {
-        uuid: 'sdfsdfsdfsd',
-        email: 's21058',
-        name: '김성훈',
-        grade: 2,
-        classNum: 1,
-        number: 3,
-        profileImg: '',
-      },
-    ])
-  )
+const data = [
+  {
+    uuid: 'sdjfksdlfjskf',
+    email: 's21025',
+    name: '김준',
+    grade: 2,
+    classNum: 1,
+    number: 5,
+    profileImg: '',
+  },
+  {
+    uuid: 'sdfsdfsdfsd',
+    email: 's21058',
+    name: '김성훈',
+    grade: 2,
+    classNum: 1,
+    number: 3,
+    profileImg: '',
+  },
+]
+
+const getSearchUser = rest.get(serverApi('/user/search'), (req, res, ctx) => {
+  const queryName = req.url.searchParams.get('name')
+  if (!queryName) return res(ctx.status(400))
+
+  return res(ctx.json(data.filter((i) => i.name.includes(queryName))))
 })
 
-export default getMain
+export default getSearchUser
--- mocks/handlers/index.ts
+++ mocks/handlers/index.ts
@@ -1,4 +1,5 @@
 import getMain from './api/get'
 import getUser from './api/user/get'
+import getSearchUser from './api/user/search/get'
 
-export const handlers = [getUser, getMain]
+export const handlers = [getUser, getMain, getSearchUser]

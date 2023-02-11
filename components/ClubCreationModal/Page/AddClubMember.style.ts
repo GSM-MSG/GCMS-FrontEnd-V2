8ea5ff960ac2c3eeb2c9f4830b8dea79e14b6d10
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-02-11 20:39:56 +0900

    Add member delay query

---
+++ components/ClubCreationModal/Page/AddClubMember.style.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled'
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  overflow-y: scroll;
+  gap: 1.5rem;
+`
--- components/ClubCreationModal/Page/AddClubMember.tsx
+++ components/ClubCreationModal/Page/AddClubMember.tsx
@@ -1,21 +1,50 @@
+import { useFetch } from '@/hooks'
+import { RootState } from '@/store'
+import { MemberType } from '@/type/common'
+import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { useSelector } from 'react-redux'
 import Input from '../Common/Input'
-import Layout from '../Common/Layout'
 import Member from '../Common/Member'
+import SubmitButton from '../Common/SubmitButton'
+import * as S from './AddClubMember.style'
 
 const AddClubMember = () => {
-  const onSubmit = async () => {
-    return
-  }
+  const { type } = useSelector((state: RootState) => ({
+    type: state.clubCreation.type,
+  }))
+  const { register, watch } = useForm<{ name: string }>()
+  const { fetch, data } = useFetch<MemberType[]>({
+    url: `/user/search?name=${watch('name')}&type=${type}`,
+    method: 'get',
+  })
+
+  useEffect(() => {
+    if (!watch('name').trim()) return
+
+    const delayFetch = setTimeout(() => {
+      fetch()
+    }, 300)
+
+    return () => clearTimeout(delayFetch)
+  }, [fetch, watch])
 
   return (
-    <Layout onSubmit={onSubmit} back complete>
-      <Input
-        label='동아리원'
-        placeholder='너, 내 동료가 돼라'
-        optional
-        description='동아리원은 나중에 다시 선택 혹은 수정이 가능해요.'
-      />
-    </Layout>
+    <S.Wrapper>
+      <div>
+        <Input
+          label='동아리원'
+          placeholder='너, 내 동료가 돼라'
+          optional
+          description='동아리원은 나중에 다시 선택 혹은 수정이 가능해요.'
+          register={register('name')}
+        />
+
+        {watch('name')?.trim() &&
+          data?.map((i) => <Member key={i.uuid} member={i} />)}
+      </div>
+      <SubmitButton />
+    </S.Wrapper>
   )
 }
 

f979c10fa160947bd3b1615123df4b2dc229eeb8
jjaejjae12 <81688137+jjaejjae12@users.noreply.github.com>
2023-02-11 17:57:05 +0900

    Delete Token.tsx file

--- lib/Token.tsx
+++
@@ -1,4 +0,0 @@
-export const accessToken = 'GCMS-accessToken'
-export const refreshToken = 'GCMS-refreshToken'
-export const accessExp = 'GCMS-accessExp'
-export const refreshExp = 'GCMS-refreshExp'

c063b490d4cf9f4a962c9900e83e558d7f2ae034
jjaejjae12 <81688137+jjaejjae12@users.noreply.github.com>
2023-02-16 03:18:38 +0900

    Update detail page effectiveness

--- components/DetailPage/index.tsx
+++ components/DetailPage/index.tsx
@@ -2,6 +2,7 @@ import { useFetch } from '@/hooks'
 import { RootState } from '@/store'
 import { setClubDetail } from '@/store/clubDetail'
 import { ClubDetailType } from '@/type/common'
+import { AxiosError } from 'axios'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
@@ -27,32 +28,34 @@ export default function DetailPage() {
     onSuccess: (data) => {
       dispatch(setClubDetail(data))
     },
+    onFailure: (e) => {
+      if (!(e instanceof AxiosError)) return console.log('unkonwn error')
+      if (e.response?.status === 404) console.log('동아리가 존재하지 않은 경우')
+    },
   })
 
   useEffect(() => {
-    setTimeout(() => {
-      fetch()
-    }, 1000)
+    fetch()
   }, [clubID])
 
-  if (!clubID || !data) return
-
   return (
-    <S.Layout>
-      <S.Wrapper>
-        <S.Section>
-          <S.ClubBanner src={clubDetail.bannerImg} />
-          <S.ClubInfo>
-            <h3>MSG(맛소금)</h3>
-            <Contact />
-            <Description />
-          </S.ClubInfo>
-          <ClubActivity />
-          <ClubMember />
-        </S.Section>
-        <SideBar />
-      </S.Wrapper>
-      <S.Footer />
-    </S.Layout>
+    data && (
+      <S.Layout>
+        <S.Wrapper>
+          <S.Section>
+            <S.ClubBanner src={clubDetail.bannerImg} />
+            <S.ClubInfo>
+              <h3>MSG(맛소금)</h3>
+              <Contact />
+              <Description />
+            </S.ClubInfo>
+            <ClubActivity />
+            <ClubMember />
+          </S.Section>
+          <SideBar />
+        </S.Wrapper>
+        <S.Footer />
+      </S.Layout>
+    )
   )
 }

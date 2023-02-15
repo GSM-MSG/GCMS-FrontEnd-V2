9b9fe9915fc804158da8630ecb3649b985dfdcec
jjaejjae12 <81688137+jjaejjae12@users.noreply.github.com>
2023-02-15 16:50:10 +0900

    Fix useLogin hook

--- hooks/useLogin.tsx
+++ hooks/useLogin.tsx
@@ -1,31 +1,23 @@
-import { accessExp, accessToken, refreshExp, refreshToken } from '@/lib/token'
-import { TokensType } from '@/type/api/TokenType'
 import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 import useFetch from './useFetch'
 
 const useLogin = () => {
   const router = useRouter()
   const gauthCode = router.query.code?.toString()
-
-  const { fetch } = useFetch<TokensType>({
+  const { fetch } = useFetch({
     url: 'auth',
     method: 'post',
-    onSuccess: (data) => {
-      if (!data) return
-      localStorage.setItem(accessToken, data.accessToken)
-      localStorage.setItem(refreshToken, data.refreshToken)
-      localStorage.setItem(accessExp, data.accessExp)
-      localStorage.setItem(refreshExp, data.refreshExp)
-      router.replace(`${process.env.NEXT_PUBLIC_GAUTH_REDIRECT_URI}`)
+    onSuccess: () => {
+      router.push('')
     },
   })
 
-  const handleLogin = () => {
+  useEffect(() => {
     if (!gauthCode) return
     fetch({ code: gauthCode })
-  }
-  return { handleLogin }
+  }, [gauthCode])
 }
 
 export default useLogin

79c42f83505c634f7aa791b1ce937499d19b6767
kangkong <kang3530554@gmail.com>
2023-02-16 15:22:47 +0900

    Add msw post applicant accept and reject

--- mocks/handlers/index.ts
+++ mocks/handlers/index.ts
@@ -2,7 +2,9 @@ import postAddClub from './api/club/post'
 import getMain from './api/get'
 import getUser from './api/user/get'
 import getSearchUser from './api/user/search/get'
-import getApplicantList from './api/applicant/get'
+import getApplicantList from './api/applicant/[clubID]/get'
+import postAcceptApplicant from './api/applicant/[clubID]/accept/post'
+import postRejectApplicant from './api/applicant/[clubID]/reject/post'
 
 export const handlers = [
   getUser,
@@ -10,4 +12,6 @@ export const handlers = [
   getSearchUser,
   postAddClub,
   getApplicantList,
+  postAcceptApplicant,
+  postRejectApplicant,
 ]

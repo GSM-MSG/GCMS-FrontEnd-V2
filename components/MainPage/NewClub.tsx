76b9a2cda1b61f37a66d5a5a16e0dba088e03520
jjaejjae12 <81688137+jjaejjae12@users.noreply.github.com>
2023-02-16 15:36:46 +0900

    Add newclub hook

--- components/MainPage/NewClub.tsx
+++ components/MainPage/NewClub.tsx
@@ -2,31 +2,22 @@ import Link from 'next/link'
 import NewClubItem from './NewClubItem'
 import * as S from './style'
 import * as SVG from '@/assets/svg'
-import { useFetch } from '@/hooks'
-import { useEffect } from 'react'
-import { ClubListType } from '@/type/common'
+import useNewClub from '@/hooks/useNewClub'
 
 export default function NewClub() {
-  const { fetch, data } = useFetch<ClubListType[]>({
-    url: `/club`,
-    method: 'get',
-  })
-
-  useEffect(() => {
-    fetch()
-  }, [])
+  const { newClubs } = useNewClub()
 
   return (
     <S.NewClubWrapper>
       <S.NewClubHead>
         <p>새로 생긴 동아리들</p>
         <Link href='/new'>
-          전체 {data?.length}개 <SVG.RightArrowIcon />
+          전체 {newClubs?.length}개 <SVG.RightArrowIcon />
         </Link>
       </S.NewClubHead>
       <S.NewClubList>
-        {data &&
-          data.map((i) => {
+        {newClubs &&
+          newClubs.map((i) => {
             return <NewClubItem key={i.id} club={i} />
           })}
       </S.NewClubList>
--- components/NewClubPage/index.tsx
+++ components/NewClubPage/index.tsx
@@ -1,16 +1,21 @@
+import useNewClub from '@/hooks/useNewClub'
 import ClubList from '../ClubList'
 import * as S from './style'
 
 export default function NewClubPage() {
+  const { newClubs } = useNewClub()
+
   return (
-    <S.Section>
-      <S.Wrapper>
-        <S.Title>
-          <h1>새로 생긴 동아리들</h1>
-          <p>전체 14개</p>
-        </S.Title>
-        <ClubList />
-      </S.Wrapper>
-    </S.Section>
+    newClubs && (
+      <S.Section>
+        <S.Wrapper>
+          <S.Title>
+            <h1>새로 생긴 동아리들</h1>
+            <p>전체 {newClubs?.length}개</p>
+          </S.Title>
+          <ClubList />
+        </S.Wrapper>
+      </S.Section>
+    )
   )
 }

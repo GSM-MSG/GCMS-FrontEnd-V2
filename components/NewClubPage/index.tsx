5622e01443b4abee2ad0313cbe8e406876a9c17b
jjaejjae12 <81688137+jjaejjae12@users.noreply.github.com>
2023-02-16 15:46:55 +0900

    Add newClubsCount

--- components/NewClubPage/index.tsx
+++ components/NewClubPage/index.tsx
@@ -4,18 +4,17 @@ import * as S from './style'
 
 export default function NewClubPage() {
   const { newClubs } = useNewClub()
+  const newClubsCount = newClubs.length
 
   return (
-    newClubs && (
-      <S.Section>
-        <S.Wrapper>
-          <S.Title>
-            <h1>새로 생긴 동아리들</h1>
-            <p>전체 {newClubs?.length}개</p>
-          </S.Title>
-          <ClubList />
-        </S.Wrapper>
-      </S.Section>
-    )
+    <S.Section>
+      <S.Wrapper>
+        <S.Title>
+          <h1>새로 생긴 동아리들</h1>
+          <p>전체 {newClubsCount}개</p>
+        </S.Title>
+        <ClubList />
+      </S.Wrapper>
+    </S.Section>
   )
 }

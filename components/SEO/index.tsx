76d298b93bb2ea5461cc9a53597bd2ff4bee9198
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-02-19 22:09:01 +0900

    Add seo url image

--- components/SEO/index.tsx
+++ components/SEO/index.tsx
@@ -3,8 +3,8 @@ import Head from 'next/head'
 interface Props {
   title: string
   description: string
-  url: string
-  image: string
+  url?: string
+  image?: string
 }
 
 const SEO = ({ title, description, url, image }: Props) => {
@@ -15,20 +15,27 @@ const SEO = ({ title, description, url, image }: Props) => {
         name='keywords'
         content='GCMS,gcms,광주소프트웨어마이스터고등학교,동아리,관리,gsm'
       />
+      <meta name='description' content={description} />
 
       {/* twitter card */}
-      <meta name='og:title' content={title} />
-      <meta name='og:site_name' content='GCMS - gsm 동아리 관리' />
-      <meta name='og:description' content={description} />
-      <meta name='og:type' content='website' />
-      <meta name='og:url' content={url} />
-      <meta property='og:image' content={image} />
+      <meta property='og:title' content={title} />
+      <meta property='og:site_name' content='GCMS - gsm 동아리 관리' />
+      <meta property='og:description' content={description} />
+      <meta property='og:type' content='website' />
+      <meta
+        property='og:url'
+        content={url ?? process.env.NEXT_PUBLIC_GAUTH_REDIRECT_URI}
+      />
+      <meta property='og:image' content={image ?? '/png/CardImage.png'} />
 
       <meta name='twitter:title' content={title} />
+      <meta name='twitter:description' content={description} />
       <meta name='twitter:card' content='summary' />
-      <meta name='twitter:site' content={url} />
-      <meta name='twitter:image' content={image} />
-      <meta property='twitter:description' content={description} />
+      <meta
+        name='twitter:site'
+        content={url ?? process.env.NEXT_PUBLIC_GAUTH_REDIRECT_URI}
+      />
+      <meta name='twitter:image' content={image ?? '/png/CardImage.png'} />
     </Head>
   )
 }

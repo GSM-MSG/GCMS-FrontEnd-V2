198ef5a5e8ea357570506975aa602179809065dd
jjaejjae12 <81688137+jjaejjae12@users.noreply.github.com>
2023-02-21 22:10:45 +0900

    Update get user logic

--- hooks/useLogin.tsx
+++ hooks/useLogin.tsx
@@ -2,12 +2,13 @@ import TokenManager from '@/api/TokenManager'
 import { TokensType } from '@/type/api/TokenManager'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
-
 import useFetch from './useFetch'
+import useUser from './useUser'
 
 const useLogin = () => {
   const router = useRouter()
   const gauthCode = router.query.code?.toString()
+  const { fetchUser } = useUser()
   const { fetch } = useFetch<TokensType>({
     url: 'auth',
     method: 'post',
@@ -16,6 +17,7 @@ const useLogin = () => {
         const tokenManager = new TokenManager()
         tokenManager.setTokens(data)
       }
+      fetchUser()
       router.push('')
     },
     onFailure: () => {

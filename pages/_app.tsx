9b3ec6d1e90ac45f2033fc2a223c0e867753aaf5
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-02-22 13:18:40 +0900

    Add next-redux-wrapper dependency

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,19 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import InitMocks from '@/mocks'
-import store from '@/store'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { Provider } from 'react-redux'
+import wrapper from '@/store'
 
 InitMocks()
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, ...rest }: AppProps) {
+  const { store, props } = wrapper.useWrappedStore(rest)
   return (
     <>
       <Provider store={store}>
-        <Component {...pageProps} />
+        <Component {...props.pageProps} />
       </Provider>
       <ToastContainer />
     </>

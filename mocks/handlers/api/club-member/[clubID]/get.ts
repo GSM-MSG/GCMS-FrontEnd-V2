24ca9165c956f3c4e0c41bcea62057bf48631de9
kangkong <kang3530554@gmail.com>
2023-02-22 23:06:26 +0900

    Add msw post and patch club-member/[clubID]

--- mocks/handlers/api/club-member/[clubID]/get.ts
+++ mocks/handlers/api/club-member/[clubID]/get.ts
@@ -1,14 +1,14 @@
+import serverApi from '@/mocks/handlers/serverApi'
 import { MemberListType } from '@/type/common'
 import { rest } from 'msw'
-import serverApi from '../../../serverApi'
 
-const data: MemberListType = {
+const headData: MemberListType = {
   scope: 'HEAD',
   clubMember: [
     {
-      uuid: '342432',
-      email: 's2012',
-      name: '이름',
+      uuid: '12038',
+      email: 's21001',
+      name: '김유저',
       grade: 1,
       classNum: 1,
       number: 1,
@@ -17,44 +17,136 @@ const data: MemberListType = {
       scope: 'HEAD',
     },
     {
-      uuid: '34224322',
-      email: 's2012',
-      name: '이르음',
+      uuid: '323432',
+      email: 's21002',
+      name: '이유저',
       grade: 1,
       classNum: 1,
-      number: 1,
-      profileImg: '',
-      scope: 'HEAD',
+      number: 2,
+      scope: 'MEMBER',
     },
     {
-      uuid: '342234432',
-      email: 's2012',
-      name: '이름',
+      uuid: '123',
+      email: 's21003',
+      name: '박유저',
+      grade: 1,
+      classNum: 2,
+      number: 10,
+      profileImg:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkZ2tjhm-hB80ErkhPt0zxaYo6iKW0tsKPdQ&usqp=CAU',
+      scope: 'MEMBER',
+    },
+    {
+      uuid: '4',
+      email: 's21004',
+      name: '최유저',
+      grade: 1,
+      classNum: 3,
+      number: 12,
+      scope: 'MEMBER',
+    },
+    {
+      uuid: '209318939',
+      email: 's21001',
+      name: '강유저',
+      grade: 2,
+      classNum: 1,
+      number: 2,
+      profileImg:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkZ2tjhm-hB80ErkhPt0zxaYo6iKW0tsKPdQ&usqp=CAU',
+      scope: 'MEMBER',
+    },
+    {
+      uuid: '19283',
+      email: 's21002',
+      name: '임유저',
+      grade: 2,
+      classNum: 3,
+      number: 18,
+      scope: 'MEMBER',
+    },
+    {
+      uuid: '10293',
+      email: 's21003',
+      name: '제갈유저',
+      grade: 3,
+      classNum: 2,
+      number: 10,
+      profileImg:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkZ2tjhm-hB80ErkhPt0zxaYo6iKW0tsKPdQ&usqp=CAU',
+      scope: 'MEMBER',
+    },
+    {
+      uuid: '314253',
+      email: 's21004',
+      name: '구유저',
+      grade: 3,
+      classNum: 3,
+      number: 14,
+      scope: 'MEMBER',
+    },
+  ],
+}
+
+const memberData: MemberListType = {
+  scope: 'MEMBER',
+  clubMember: [
+    {
+      uuid: '12038',
+      email: 's21001',
+      name: '김유저',
       grade: 1,
       classNum: 1,
       number: 1,
       profileImg:
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkZ2tjhm-hB80ErkhPt0zxaYo6iKW0tsKPdQ&usqp=CAU',
-      scope: 'HEAD',
+      scope: 'MEMBER',
     },
     {
-      uuid: '342244322',
-      email: 's2012',
-      name: '이르음',
+      uuid: '323432',
+      email: 's21002',
+      name: '이유저',
       grade: 1,
       classNum: 1,
-      number: 1,
+      number: 2,
       profileImg: '',
-      scope: 'HEAD',
+      scope: 'MEMBER',
+    },
+    {
+      uuid: '123',
+      email: 's21003',
+      name: '박유저',
+      grade: 1,
+      classNum: 2,
+      number: 10,
+      profileImg:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkZ2tjhm-hB80ErkhPt0zxaYo6iKW0tsKPdQ&usqp=CAU',
+      scope: 'MEMBER',
+    },
+    {
+      uuid: '4',
+      email: 's21004',
+      name: '최유저',
+      grade: 1,
+      classNum: 3,
+      number: 12,
+      profileImg: '',
+      scope: 'MEMBER',
     },
   ],
 }
 
 const getClubMemberList = rest.get(
-  serverApi('/club-member:/clubID'),
-  async (req, res, ctx) => {
-    if (!req.params.clubID) return res(ctx.status(400))
-    return res(ctx.status(200), ctx.json(data))
+  serverApi('/club-member/:clubID'),
+  (req, res, ctx) => {
+    switch (req.params.clubID) {
+      case '1':
+        return res(ctx.status(200), ctx.json(headData))
+      case '2':
+        return res(ctx.status(200), ctx.json(memberData))
+      default:
+        return res(ctx.status(400))
+    }
   }
 )
 

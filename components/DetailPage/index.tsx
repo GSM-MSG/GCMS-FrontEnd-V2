4cb5a0bb4f41f87e094208087234c10a655fb510
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-02-22 13:38:37 +0900

    Add SSR in detail page

--- components/DetailPage/index.tsx
+++ components/DetailPage/index.tsx
@@ -1,10 +1,5 @@
-import { useFetch } from '@/hooks'
 import { RootState } from '@/store'
-import { setClubDetail } from '@/store/clubDetail'
-import { ClubDetailType } from '@/type/common'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import SEO from '../SEO'
 import ClubActivity from './ClubActivity'
 import ClubMember from './ClubMember'
@@ -15,37 +10,18 @@ import SideBar from './SideBar'
 import * as S from './style'
 
 export default function DetailPage() {
-  const router = useRouter()
-  const clubID = router.query.clubID?.toString()
-  const dispatch = useDispatch()
-
   const { clubDetail } = useSelector((state: RootState) => ({
     clubDetail: state.clubDetail,
   }))
 
-  const { fetch, data } = useFetch<ClubDetailType>({
-    url: `/club/${clubID}`,
-    method: 'get',
-    onSuccess: (data) => {
-      dispatch(setClubDetail(data))
-    },
-    errors: {
-      400: '동아리 아이디를 찾을수 없습니다.',
-    },
-  })
-
-  useEffect(() => {
-    if (clubID) fetch()
-  }, [clubID])
-
   return (
     <>
       <SEO
-        title={`GCMS | ${data?.name}`}
-        description={data?.content}
-        image={data?.bannerImg}
+        title={`GCMS | ${clubDetail.name}`}
+        description={clubDetail.content}
+        image={clubDetail.bannerImg}
       />
-      {data && (
+      {clubDetail.id && (
         <S.Layout>
           <S.Wrapper>
             <S.Section>

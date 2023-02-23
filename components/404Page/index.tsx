5dc7f4ce9b5270518cbce074a987f841a6114ac0
kangkong <kang3530554@gmail.com>
2023-02-23 18:09:12 +0900

    Add 404 page

---
+++ components/404Page/index.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import * as S from './style'
+
+export default function Error404Page() {
+  return (
+    <S.Positioner>
+      <S.Layer>
+        <S.TextBox>
+          <Image src='/png/404.png' alt='404' width='237' height='94' />
+          <p>
+            요청하신 페이지 주소가 잘못되었거나 변경 혹은 삭제되었을수도 있어요
+          </p>
+        </S.TextBox>
+        <S.HomeBtn href='/'>
+          <span />
+          홈으로
+        </S.HomeBtn>
+      </S.Layer>
+    </S.Positioner>
+  )
+}
---
+++ pages/404/index.tsx
@@ -0,0 +1,5 @@
+import Error404Page from '@/components/404Page'
+
+export default function Error404() {
+  return <Error404Page />
+}

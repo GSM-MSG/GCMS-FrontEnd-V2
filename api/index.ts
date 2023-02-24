9442499e60dc88fd9efcef5b3a46badf31be7738
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-02-24 19:02:44 +0900

    Add cancel token feature

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,13 @@
 import axios from 'axios'
 import TokenManager from './TokenManager'
 
+const CancelToken = axios.CancelToken
+const source = CancelToken.source()
+
 const API = axios.create({
   baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
   withCredentials: true,
+  cancelToken: source.token,
 })
 
 API.interceptors.request.use(async (config) => {

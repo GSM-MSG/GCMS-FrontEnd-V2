f89aa4efef62c44985974b6a6cfe0c9a62898f70
kangkong <kang3530554@gmail.com>
2023-02-27 21:20:04 +0900

    Fix change profile img logic

--- components/MyPage/index.tsx
+++ components/MyPage/index.tsx
@@ -1,46 +1,48 @@
 import * as SVG from '@/assets/svg'
-import { useFetch } from '@/hooks'
-import { ProfileType } from '@/type/common'
+import { useLoggedIn } from '@/hooks'
+import { RootState } from '@/store'
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
 import SEO from '../SEO'
 import ClubItem from './ClubItem'
 import ProfileSetting from './ProfileSetting'
 import * as S from './style'
 
 export default function MyPage() {
-  const { fetch, data } = useFetch<ProfileType>({
-    url: '/user',
-    method: 'get',
-  })
   const [isSetting, setSetting] = useState<boolean>(false)
+  const { fetchUser } = useLoggedIn({})
 
-  useEffect(() => {
-    fetch()
-  }, [])
+  const { user } = useSelector((state: RootState) => ({
+    user: state.user,
+  }))
+
+  const onSubmit = () => {
+    fetchUser()
+  }
 
   return (
     <>
-      <SEO title={`GCMS | ${data?.name}`} />
+      <SEO title={`GCMS | ${user?.name}`} />
       <S.Positionier>
         <S.Layer>
           <S.ProfileBox>
             <SVG.ProfileIcon />
             <S.ProfileContent>
               <S.ProfileImg>
-                {data?.profileImg && (
+                {user?.profileImg && (
                   <Image
-                    src={data.profileImg}
+                    src={user.profileImg}
                     alt='profileImg'
                     width={60}
                     height={60}
                   />
                 )}
               </S.ProfileImg>
-              <p>{data?.name}님</p>
+              <p>{user?.name}님</p>
               <small>
-                {data?.grade ?? 0}학년 {data?.classNum ?? 0}반{' '}
-                {data?.number ?? 0}번
+                {user?.grade ?? 0}학년 {user?.classNum ?? 0}반{' '}
+                {user?.number ?? 0}번
               </small>
             </S.ProfileContent>
             <S.SettingButton onClick={() => setSetting(!isSetting)}>
@@ -52,18 +54,18 @@ export default function MyPage() {
               <h2>내 동아리</h2>
               <S.ClubContainer>
                 <S.ClubType>전공동아리</S.ClubType>
-                <ClubItem clubType='MAJOR' data={data} />
+                <ClubItem clubType='MAJOR' data={user} />
               </S.ClubContainer>
               <S.ClubContainer>
                 <S.ClubType>자율동아리</S.ClubType>
-                <ClubItem clubType='FREEDOM' data={data} />
+                <ClubItem clubType='FREEDOM' data={user} />
               </S.ClubContainer>
               <S.ClubContainer>
                 <S.ClubType>사설동아리</S.ClubType>
-                <ClubItem clubType='EDITORIAL' data={data} />
+                <ClubItem clubType='EDITORIAL' data={user} />
               </S.ClubContainer>
             </S.ClubBox>
-            {isSetting && <ProfileSetting />}
+            {isSetting && <ProfileSetting onSubmit={() => onSubmit()} />}
           </S.ContentBox>
         </S.Layer>
       </S.Positionier>

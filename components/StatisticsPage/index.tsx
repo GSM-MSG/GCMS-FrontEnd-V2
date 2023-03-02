5ece620e2e0492a70b535c6d1ac38d76b9e1dd89
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-03-02 20:39:55 +0900

    Add Statistics component

---
+++ components/StatisticsPage/index.tsx
@@ -0,0 +1,7 @@
+import * as S from './style'
+
+const StatisticsPage = () => {
+  return <S.Wrapper>hello</S.Wrapper>
+}
+
+export default StatisticsPage
---
+++ components/StatisticsPage/style.ts
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled'
+
+export const Wrapper = styled.div`
+  width: 43.75rem;
+  margin: 0 auto;
+`

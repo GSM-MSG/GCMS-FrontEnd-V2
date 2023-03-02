113e1c3d0da461ba06a5ff1687e2626a17a37546
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-03-03 00:24:56 +0900

    Add excel print button

--- components/StatisticsPage/index.tsx
+++ components/StatisticsPage/index.tsx
@@ -1,10 +1,17 @@
 import Statistics from './Statistics'
 import * as S from './style'
+import * as SVG from '@/assets/svg'
 
 const StatisticsPage = () => {
   return (
     <S.Wrapper>
-      <Statistics />
+      <S.Top>
+        <Statistics />
+        <S.ExcelIcon>
+          <SVG.PrinterIcon />
+          엑셀 출력
+        </S.ExcelIcon>
+      </S.Top>
     </S.Wrapper>
   )
 }
--- components/StatisticsPage/style.ts
+++ components/StatisticsPage/style.ts
@@ -5,3 +5,26 @@ export const Wrapper = styled.div`
   width: 90%;
   margin: 3rem auto;
 `
+
+export const Top = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: 1rem;
+  flex-wrap: wrap;
+
+  @media (max-width: 550px) {
+    justify-content: center;
+  }
+`
+
+export const ExcelIcon = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+  color: rgba(255, 255, 255, 0.3);
+  padding: 0.7rem;
+  border-radius: 0.75rem;
+  background: #242425;
+  cursor: pointer;
+`

c65c0cc6d870ed22aa8cd1b452809c1a996392c9
kangkong <kang3530554@gmail.com>
2023-03-06 02:46:10 +0900

    Use usefetch instead of dummy data

--- components/Common/ClubPermissionList/index.tsx
+++ components/Common/ClubPermissionList/index.tsx
@@ -1,8 +1,9 @@
 import { SearchIcon } from '@/assets/svg'
-import { ClubType } from '@/type/common'
+import { useFetch } from '@/hooks'
+import { ClubListType, ClubType } from '@/type/common'
+import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import Input from '../Input'
-import { ClubObj } from './ClubObj'
 import * as S from './style'
 
 interface Props {
@@ -11,6 +12,15 @@ interface Props {
 
 const ClubPermissionList = ({ inputLabel }: Props) => {
   const { register, watch } = useForm({ defaultValues: { value: '' } })
+
+  const { fetch, data } = useFetch<ClubListType[]>({
+    url: '/admin',
+    method: 'GET',
+  })
+
+  useEffect(() => {
+    fetch()
+  }, [])
 
   const ClubType = (type: ClubType) => {
     switch (type) {
@@ -40,30 +50,32 @@ const ClubPermissionList = ({ inputLabel }: Props) => {
           <small>이름</small>
           <small>설명</small>
         </S.Header>
-        {ClubObj.filter((i) => {
-          return i.name.includes(watch('value').trim())
-        }).map((item) => {
-          return (
-            <S.ClubWrapper key={item.id}>
-              <S.ClubBox>
-                <S.ClubImgBox>
-                  <S.Img src={item.bannerImg} alt='bannerImg' />
-                </S.ClubImgBox>
-                <S.ClubInfo>
-                  <S.ClubKind>{ClubType(item.type)}</S.ClubKind>
-                  <S.ClubName href={`/detail/${item.id}`}>
-                    {item.name}
-                  </S.ClubName>
-                  <S.ClubDescription>{item.content}</S.ClubDescription>
-                </S.ClubInfo>
-                <S.OptionBox>
-                  <S.OptionBtn>허락</S.OptionBtn>
-                  <S.OptionBtn>거부</S.OptionBtn>
-                </S.OptionBox>
-              </S.ClubBox>
-            </S.ClubWrapper>
-          )
-        })}
+        {data
+          ?.filter((i) => {
+            return i.name.includes(watch('value').trim())
+          })
+          .map((item) => {
+            return (
+              <S.ClubWrapper key={item.id}>
+                <S.ClubBox>
+                  <S.ClubImgBox>
+                    <S.Img src={item.bannerImg} alt='bannerImg' />
+                  </S.ClubImgBox>
+                  <S.ClubInfo>
+                    <S.ClubKind>{ClubType(item.type)}</S.ClubKind>
+                    <S.ClubName href={`/detail/${item.id}`}>
+                      {item.name}
+                    </S.ClubName>
+                    <S.ClubDescription>{item.content}</S.ClubDescription>
+                  </S.ClubInfo>
+                  <S.OptionBox>
+                    <S.OptionBtn>허가</S.OptionBtn>
+                    <S.OptionBtn>거부</S.OptionBtn>
+                  </S.OptionBox>
+                </S.ClubBox>
+              </S.ClubWrapper>
+            )
+          })}
       </S.ClubContainer>
     </S.Layer>
   )

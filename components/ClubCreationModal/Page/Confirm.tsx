94dd9c4f6836a88c4d1a2846b1598123f1ca8966
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-03-09 09:58:45 +0900

    Add Confirm publishing

--- components/ClubCreationModal/Page/Confirm.tsx
+++ components/ClubCreationModal/Page/Confirm.tsx
@@ -1,7 +1,21 @@
 import * as S from './Confirm.style'
+import * as SVG from '@/assets/svg'
+import SubmitButton from '../Common/SubmitButton'
 
 const Confirm = () => {
-  return <S.Wrapper></S.Wrapper>
+  return (
+    <S.Wrapper>
+      <S.Content>
+        <SVG.Envelope />
+        <S.Title>동아리 개설이 신청되었습니다</S.Title>
+        <S.Description>
+          어드민이 동아리 개설 신청을 수락한 후에 동아리가 개설돼요
+        </S.Description>
+      </S.Content>
+
+      <SubmitButton />
+    </S.Wrapper>
+  )
 }
 
 export default Confirm

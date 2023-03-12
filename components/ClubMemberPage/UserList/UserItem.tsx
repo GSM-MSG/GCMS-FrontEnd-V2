0a5bfcc3e14407b287d06618c23fb3a610eb9a92
kangkong <kang3530554@gmail.com>
2023-03-13 01:50:24 +0900

    Rename attribute name

--- components/ClubMemberPage/UserList/UserItem.tsx
+++ components/ClubMemberPage/UserList/UserItem.tsx
@@ -32,7 +32,7 @@ export default function UserItem({ item, scope }: MemberItemProps) {
   }
   return (
     <S.UserWrapper>
-      <S.UserBox selected={isSelected}>
+      <S.UserBox select={isSelected}>
         <S.UserImgBox>
           {item.profileImg && <S.Img src={item.profileImg} alt='profileImg' />}
         </S.UserImgBox>

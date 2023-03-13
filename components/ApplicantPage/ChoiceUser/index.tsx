adaf6119ac7bcea5c5d801565b4aeaf924a9ae90
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-03-13 13:50:27 +0900

    Fix confirm in reject, accept

--- components/ApplicantPage/ChoiceUser/index.tsx
+++ components/ApplicantPage/ChoiceUser/index.tsx
@@ -1,8 +1,9 @@
+import ConfirmModal from '@/components/Common/ConfirmModal'
 import { useFetch } from '@/hooks'
 import { RootState } from '@/store'
 import { removeUser } from '@/store/applicant'
 import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import * as S from './style'
 
@@ -16,6 +17,7 @@ export default function ChoiceUser({ onSubmit }: choiceUserProps) {
   }))
   const dispatch = useDispatch()
   const [choice, setChoice] = useState('')
+  const [isShow, setIsShow] = useState<boolean>(false)
   const router = useRouter()
   const { fetch: data } = useFetch({
     url: `/applicant/${router.query.clubID}/${choice}`,
@@ -23,23 +25,36 @@ export default function ChoiceUser({ onSubmit }: choiceUserProps) {
     onSuccess: onSubmit,
   })
 
-  const onFetch = () => {
+  const onClick = (choice: string) => {
+    setChoice(choice)
+    setIsShow(true)
+  }
+
+  const onConfirm = () => {
     applicant.forEach(async (item) => {
       await data({ uuid: item.uuid })
       dispatch(removeUser(item.uuid))
     })
   }
 
-  useEffect(() => {
-    if (choice === '') return
-    return onFetch()
-  }, [choice])
   return (
-    <S.Positioner>
-      <S.Layer>
-        <S.RejectBtn onClick={() => setChoice('reject')}>불합격</S.RejectBtn>
-        <S.AcceptBtn onClick={() => setChoice('accept')}>합격</S.AcceptBtn>
-      </S.Layer>
-    </S.Positioner>
+    <>
+      <S.Positioner>
+        <S.Layer>
+          <S.RejectBtn onClick={() => onClick('reject')}>불합격</S.RejectBtn>
+          <S.AcceptBtn onClick={() => onClick('accept')}>합격</S.AcceptBtn>
+        </S.Layer>
+      </S.Positioner>
+      {isShow && (
+        <ConfirmModal
+          title={`지원자 ${choice === 'accept' ? '합격' : '불합격'}`}
+          description={`정말 ${
+            choice === 'accept' ? '합격' : '불합격'
+          }시키겠습니까?`}
+          onClose={() => setIsShow(false)}
+          onConfirm={onConfirm}
+        />
+      )}
+    </>
   )
 }

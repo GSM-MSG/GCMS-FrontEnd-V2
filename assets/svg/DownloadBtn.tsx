eac6dd34f443e3c509efab56aacb67c596dfaaef
unknown <momlin069@gmail.com>
2023-07-11 23:30:34 +0900

    publishing download button

---
+++ assets/svg/DownloadBtn.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const DownloadBtn = () => {
+  return (
+    <svg
+      width='16'
+      height='16'
+      viewBox='0 0 16 16'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M2.34315 7.0006L8 12.6575M8 12.6575L13.6569 7.0006M8 12.6575V1.34375'
+        stroke='white'
+        strokeWidth='1.5'
+      />
+      <path d='M2 14H14' stroke='white' strokeLinecap='round' />
+    </svg>
+  )
+}

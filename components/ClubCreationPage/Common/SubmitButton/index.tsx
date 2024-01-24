a36852a08e09311b0a30556360949f6f5d47e5eb
이아론 <aron0613@naver.com>
2024-01-24 16:34:37 +0900

    Update SubmitButton OnClick Event

--- components/ClubCreationPage/Common/SubmitButton/index.tsx
+++ components/ClubCreationPage/Common/SubmitButton/index.tsx
@@ -1,4 +1,6 @@
 import * as S from './style'
+import { backPage } from '@/store/clubCreationPage'
+import { useDispatch } from 'react-redux'
 
 export interface Props {
   backOnClick?: () => Promise<void> | void
@@ -6,10 +8,17 @@ export interface Props {
 }
 
 const SubmitButton = ({ backOnClick, nextOnClick }: Props) => {
+  const dispatch = useDispatch()
   return (
     <S.Wrapper>
-      <S.BackButton onClick={backOnClick}>이전</S.BackButton>
-      <S.SubmitButton onClick={nextOnClick}>다음</S.SubmitButton>
+      <S.BackButton
+        onClick={backOnClick ? backOnClick : () => dispatch(backPage())}
+      >
+        이전
+      </S.BackButton>
+      <S.SubmitButton onClick={nextOnClick} type='submit'>
+        다음
+      </S.SubmitButton>
     </S.Wrapper>
   )
 }

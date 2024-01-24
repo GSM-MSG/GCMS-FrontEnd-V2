231f8f57591caeac77f35b3e1d9084943d4cfb11
이아론 <aron0613@naver.com>
2024-01-24 16:33:37 +0900

    Add Layout component for ClubCreationPage

---
+++ components/ClubCreationPage/Common/Layout/style.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled'
+
+export const Wrapper = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin: 2.5rem 0;
+`
+export const Content = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`

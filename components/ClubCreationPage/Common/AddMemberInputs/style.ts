468470abaa5b9962035503b3af23afa0f2ffc3e7
이아론 <aron0613@naver.com>
2024-02-03 21:57:22 +0900

    Add MemberList ScrollBar

--- components/ClubCreationPage/Common/AddMemberInputs/style.ts
+++ components/ClubCreationPage/Common/AddMemberInputs/style.ts
@@ -5,4 +5,15 @@ export const Content = styled.div`
   max-height: 300px;
   display: flex;
   flex-direction: column;
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+  ::-webkit-scrollbar-thumb {
+    height: 30%;
+    background: #4164e1;
+    border-radius: 10px;
+  }
+  ::-webkit-scrollbar-track {
+    background: rgba(33, 122, 244, 0.1);
+  }
 `
